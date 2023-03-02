de4ff97eda0c24da304829b9daba7ce428a9174f
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2023-03-02 17:51:48 +0800

    2023-03-02
    API 完成
    開始串接

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,112 @@
-async function getAchievement(year) {
-    const res = await axios.post(`https://private-aded43-bee3054.apiary-mock.com/getAchievement?year=${year}`)
-    return res
+// 業績
+const salesRequest = axios.create({
+    baseURL: 'https://private-aded43-bee3054.apiary-mock.com/api/achievements?'
+})
+
+async function getSales(year) {
+    const res = await salesRequest.get(`year=${year}`);
+    // console.log('總業績', res.data);
+    return res.data.result
 }
 
 
-export const salesData = year => getAchievement(year)
 
-async function aa(){
-    const res = await axios.post(`https://private-aded43-bee3054.apiary-mock.com/getSalesList?month=202301`)
-    console.log(res.data);
+export const getSalesAPI = getSales
+
+// --------------------------------------------------------------------------------------------------------
+
+// 地區
+const salesByPlaceRequest = axios.create({
+    baseURL: 'https://private-aded43-bee3054.apiary-mock.com/api/salesByPlace?'
+})
+
+async function getSalesByPlace(year) {
+    const res = await salesByPlaceRequest.get(`year=${year}`);
+    // console.log('地區業績', res.data);
+    return res.data.result
 }
-aa()
+
+
+
+
+
+
+
+export const getSalesByPlaceAPI = getSalesByPlace
+
+// --------------------------------------------------------------------------------------------------------
+
+// 任務
+const missionsRequest = axios.create({
+    baseURL: 'https://private-aded43-bee3054.apiary-mock.com/api/missions?'
+})
+
+async function getMissions() {
+    const res = await missionsRequest.get()
+    // console.log('任務', res.data);
+    return res.data.result
+}
+
+
+
+
+
+export const getMissionsAPI = getMissions
+
+
+// --------------------------------------------------------------------------------------------------------
+
+// 每日回報
+const daily_reportRequest = axios.create({
+    baseURL: 'https://private-aded43-bee3054.apiary-mock.com/api/dailyReport?'
+})
+
+async function getDailyReport(day, sales) {
+    const res = await daily_reportRequest.get(`day=${day}&sales${sales}`)
+    // console.log('每日報告', res.data);
+    return res.data.result
+}
+
+
+
+export const getDailyReportAPI = getDailyReport
+
+
+// --------------------------------------------------------------------------------------------------------
+
+// 每週PPT
+const PPTrequest = axios.create({
+    baseURL: 'https://private-aded43-bee3054.apiary-mock.com/api/weekilyWork?'
+})
+
+async function getPPT(week) {
+    const res = await PPTrequest.get(`week=${week}`)
+    // console.log('每周PPT', res.data);
+    return res.data.result
+}
+
+
+
+
+
+export const getPPTAPI = getPPT
+
+// --------------------------------------------------------------------------------------------------------
+
+// 會議紀錄
+const meetingRecordRequest = axios.create({
+    baseURL: 'https://private-aded43-bee3054.apiary-mock.com/api/meetingRecord?'
+})
+
+async function getMeetingRecord() {
+    const res = await meetingRecordRequest.get()
+    // console.log('會議記錄', res.data);
+    return res.data.result
+}
+
+
+
+export const getMeetingRecordAPI = getMeetingRecord
+
+
+
--- src/js/d3.js
+++ src/js/d3.js
@@ -1,44 +1,142 @@
-import { salesData } from './api.js'
+import { getSalesAPI, getSalesByPlaceAPI, getMissionsAPI, getDailyReportAPI, getPPTAPI, getMeetingRecordAPI } from './api.js'
 
-async function getData_goal() {
-    const res = await salesData(2022);
-    const data = res.data
-    // console.log(typeof(data[0].data[0].goal));
-    let a = data[0].data
-    console.log(a);
-    let goal = a.map(item => item.goal.toLocaleString())
-    console.log(goal);
-    d3.select('#goal')
+async function setTable_sales() {
+    const data = await getSalesAPI()
+
+    // console.log(Object.values(data).slice(1));
+    let salesData = Object.values(data).slice(1)
+    // console.log({ salesData });
+
+    d3.select('.budget-target .table_goal')
         .selectAll('.table-data')
-        .data(goal)
+        .data(salesData)
         .enter()
         .append("td")
         .attr("class", 'table-data')
-        .text(d => d)
+        .text(d => d.goal.toLocaleString())
 
-    let achieve = a.map(item => item.achievement.toLocaleString())
-    console.log(achieve);
-    d3.select('#achieve')
+
+    d3.select('.budget-target .table_achieve')
         .selectAll('.table-data')
-        .data(achieve)
+        .data(salesData)
         .enter()
         .append("td")
         .attr("class", 'table-data')
         .text(d => {
-            if (d == '0') {
+            if (d.achievement == 0) {
                 return '-'
             }
-            return d
+            return d.achievement.toLocaleString()
         })
 
-    let minus = a.map(item => (item.goal - item.achievement).toLocaleString())
-    console.log(minus);
-    d3.select('#minus')
+    d3.select('.budget-target .table_gap')
         .selectAll('.table-data')
-        .data(minus)
+        .data(salesData)
         .enter()
         .append("td")
         .attr("class", 'table-data')
-        .text(d => d)
+        .text(d => {
+            if (d.achievement == 0) {
+                return '-'
+            }
+            return (d.achievement - d.goal).toLocaleString()
+        })
 }
-getData_goal()
+setTable_sales()
+
+
+
+
+async function setTable_salesByPlace() {
+    const data = await getSalesByPlaceAPI()
+
+    // console.log(data);
+    function finalData(i) {
+        let salesByPlaceData = Object.values(Object.values(data)[i]).slice(0, -1)
+        salesByPlaceData.push(salesByPlaceData[0] - salesByPlaceData[1])
+        salesByPlaceData.push((salesByPlaceData[1] / salesByPlaceData[0] * 100).toFixed(1) + '%')
+        return salesByPlaceData
+    }
+
+    function fill(place, i) {
+        const data = finalData(i)
+        // console.log(data);
+        d3.select(`.place-achievement .table_${place}`)
+            .selectAll('.table-data')
+            .data(data)
+            .enter()
+            .append("td")
+            .attr("class", 'table-data')
+            .text(d => {
+                if (d == 0 || d == 'NaN%') {
+                    return '-'
+                }
+                return d.toLocaleString()
+            })
+    }
+    fill(`Europe_A`, 0)
+    fill(`USA`, 1)
+    fill(`Europe_B`, 2)
+    fill(`Taiwan`, 3)
+    fill(`Other`, 4)
+    fill(`Japan`, 5)
+    fill(`VIP`, 6)
+    fill(`China`, 7)
+    fill(`TaiwanOEM`, 8)
+
+
+    let totalData = []
+    totalData.push(Object.values(data).map(i => i.goal).reduce((a, b) => a + b, 0))
+    totalData.push(Object.values(data).map(i => i.done).reduce((a, b) => a + b, 0))
+    totalData.push(Object.values(data).map(i => i.work_on).reduce((a, b) => a + b, 0))
+    totalData.push(Object.values(data).map(i => i.prediction).reduce((a, b) => a + b, 0))
+    totalData.push(totalData[0] - totalData[1])
+    totalData.push((totalData[1] / totalData[0] * 100).toFixed(1) + '%')
+
+    // console.log(totalData);
+    d3.select(`.place-achievement .table_Total`)
+        .selectAll('.table-data')
+        .data(totalData)
+        .enter()
+        .append("td")
+        .attr("class", 'table-data')
+        .text(d => {
+            if (d == 0 || d == 'NaN%') {
+                return '-'
+            }
+            return d.toLocaleString()
+        })
+}
+setTable_salesByPlace()
+
+
+
+async function setTable_mission() {
+    const datas = await getMissionsAPI()
+
+    console.log(datas);
+
+    datas.forEach((data,i) => {
+        console.log(Object.values(data));
+        let a = Object.values(data).slice(0,-1)
+        d3.select(`.list-by-index tbody tr`)
+            .selectAll('td')
+            .data(a)
+            .join(
+                enter => enter.text(d => {
+                    if (d == ``) {
+                        return '-'
+                    }
+                    return d
+                }),
+                update => update.text(d => {
+                    if (d == ``) {
+                        return '-'
+                    }
+                    return d
+                })
+            )
+    });
+
+}
+setTable_mission()
