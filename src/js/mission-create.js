3bd05e70e3f447341a6ad44754760fb5cf7607d2
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2023-03-08 16:28:26 +0800

    2023-03-08
    每週報告完成

--- src/js/mission-create.js
+++ src/js/mission-create.js
@@ -12,26 +12,5 @@ async function missionCreate() {
     const today = d3.timeFormat('%Y-%m-%d')(new Date())
     d3.select('.formTitle .info .mission_createDate span')
         .text(today)
-
-    let fileList=[]
-
-    const uploadConfirm = document.querySelectorAll('#file-confirm')
-
-
-    const a = document.querySelectorAll('#mission_attach')
-    const b = document.querySelectorAll('#mission_upload-attach')
-
-    a.forEach(i =>{
-        i.addEventListener('change',()=>{
-            
-        })
-    })
-
-    uploadConfirm.forEach(i => {
-        i.addEventListener('click', ()=>{
-            fileList.push('1')
-            console.log(fileList);
-        })
-    })
 }
 missionCreate()
