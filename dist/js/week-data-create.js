508e521191fc6df2913c578bb09aa227ead9cafa
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2023-03-13 17:46:47 +0800

    2023-03-13
    上傳終於成功

--- dist/js/week-data-create.js
+++ dist/js/week-data-create.js
@@ -25,21 +25,62 @@ uploadPPT.addEventListener('click', () => {
     }
     upload(file)
 })
+const uploadMeet = document.querySelector('#meeting-report label button')
+uploadMeet.addEventListener('click', () => {
+    const file = MeetingFile.files[0];
+    if (!file) {
+        return
+    }
+    upload(file)
+})
 
-const uploadFile_body = {
-    "FormId": "", //單號
-    "EmpId": EmpID, //員工工號
-    "filename": "", //檔名
-    "webName": 'wData_c', //那個頁面的命名
-    "file": "" //檔案
-}
 async function upload(file) {
-    uploadFile_body.filename = PPTfile.files[0].name;
-    uploadFile_body.file = file;
-    console.log(uploadFile_body);
-    // const res = await axios({
-    //     method: 'POST',
-    //     url: 'https://orangeapi.orange-electronic.com/api/UploadFormData',
-    //     data: uploadFile_body
-    // })
+    const filePackage = new FormData()
+    filePackage.append('FormId', 'wData' + Date.now())
+    filePackage.append('EmpId', EmpID)
+    filePackage.append('filename', file.name)
+    filePackage.append('webName', 'wData')
+    filePackage.append('file', file)
+    const uploadRes = await axios({
+        method: 'POST',
+        url: 'https://orangeapi.orange-electronic.com/api/UploadFormData',
+        data: filePackage
+    });
+    console.log(uploadRes.data);
+    console.log(filePackage.get('FormId'))
+    const body = {
+        "FileId": ""//附檔編號
+        , "FileName": "" //附檔名稱
+        , "FilePath": "" //附檔存放路徑 //ReportFile
+        , "WebName": "wData" //附檔所在網頁名稱
+        , "WebID": filePackage.get('FormId')//附檔所在 對應表單表編號ID
+        , "ExecID": ""
+    }
+    console.log(body);
+    const res = await axios({
+        method: 'POST',
+        url: 'https://orangeapi.orange-electronic.com/api/GetWFP',
+        data: body
+    });
+    const path = res.data[0].FilePath
+
+    async function go() {
+        const body = {
+            "Sales": userName//業務
+            , "PPtName": file.name//會議ppt檔名
+            , "PPtpatch": path//會議ppt路徑
+            , "MeetName": ""//會議記錄檔名
+            , "MeetPatch": ""//會議記錄路徑
+            , "id": ""//主keyid
+        }
+        console.log(body);
+        const res = await axios({
+            method: 'POST',
+            url: 'http://orangeapi.orange-electronic.com/api/SalesWeekWorkAdd',
+            data: body
+        })
+        console.log(res);
+    }
+    go()
+
 }
